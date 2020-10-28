d9f4b3ed98c6be96f4fb76f6696eac0b1d03397c
Ilman <pythonilmantest@gmail.com>
2020-10-28 22:17:56 +0530

    Created custom user profile manager and super user

---
+++ imall/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ImallConfig(AppConfig):
+    name = 'imall'
---
+++ imall/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+
+# Create your models here.
+
+class USERPROFILEMANAGER(BaseUserManager):
+    """Manage user profiles"""
+
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError("User must enter a valid email address")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)  
+        """For converting password to hash"""
+        user.save()
+
+        return user
+
+
+    def create_superuser(self, email, name, password):
+        """Create a new super user"""
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save()
+
+        return user
+
+
+class USERPROFILE(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = USERPROFILEMANAGER()
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Retrieve full name of user"""
+        return self.name
+
+    def get_short_name(self):
+        """Retrieve short name of user"""
+        return self.name
+
+
+    def __str__(self):
+        """Return string representation of our user"""
+        return self.email
+
+
+
+
